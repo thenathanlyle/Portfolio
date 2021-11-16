05bd9824f6ef0617b3df4c5f0758c10469f6f586
Nathan Lyle <brickultra@Nathans-Mac-mini.local>
2021-11-16 15:29:00 -0500

    project 3 details

--- client/src/screens/Project3.jsx
+++ client/src/screens/Project3.jsx
@@ -6,6 +6,11 @@ export default function Project3() {
     <div className="project3">
       <img className="test" src={CryptoCradle} alt="" />
       <h3>CryptoCradle</h3>
+      <p>
+        CryptoCradle is a cryptocurrency price chart using coin data from an
+        external API while allowing a user to search for their favorite
+        cryptocurrency.
+      </p>
     </div>
   );
 }
