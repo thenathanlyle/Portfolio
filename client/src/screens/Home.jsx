56a5e72c6ea1b2e6537f5f9c7c547a49a680bb94
Nathan Lyle <brickultra@Nathans-Mac-mini.local>
2022-02-08 16:11:12 -0500

    updated link

--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -37,7 +37,7 @@ export default function Home() {
             theme={theme}
             color="secondary"
             variant="outlined"
-            href="https://drive.google.com/file/d/1Bctk3Yb76otSqD_XbIZECYwkHEyi1zMc/view?usp=sharing"
+            href="https://drive.google.com/file/d/1ScTqXTx_8CWAq59sb6dvDsQAeju5x8pO/view?usp=sharing"
             target="_blank"
           >
             Resume
